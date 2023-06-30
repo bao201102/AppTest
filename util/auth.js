4d89758fa5a6aa87d485f47f8de90a110e719459
Nguyễn Ngọc Bảo <76846478+bao201102@users.noreply.github.com>
2023-06-30 14:45:14 +0700

    Update auth.js

--- util/auth.js
+++ util/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "AIzaSyBc4cCo9MCu7076i7q84VRgadoQ5aCkhfo";
+const API_KEY = "AIzaSyBc4cCo9MCu7076i7q84VRgadoQ5aCkhfo-asdfghjkl";
 
 async function authenticate(mode, email, password) {
   const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
